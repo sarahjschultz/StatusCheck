c93f5e7e7be63ec13f27715c539f40403edc0a2a
Sarah Schultz <sarahjschultz@gmail.com>
2015-03-01 22:03:27 -0800

    Final cleanup. Try to fix random failing specs. Consider FactoryGirl. But actually just stop considering alternatives and go to bed because this is long past the 4-hour deadline.

--- StatusCheck/spec/controllers/statuses_controller_spec.rb
+++ StatusCheck/spec/controllers/statuses_controller_spec.rb
@@ -12,29 +12,6 @@ RSpec.describe StatusesController, type: :controller do
     end
   end
 
-  describe "GET #show" do
-    it "assigns the requested status as @status" do
-      status = Status.create! valid_attributes
-      get :show, {:id => status.to_param}
-      expect(assigns(:status)).to eq(status)
-    end
-  end
-
-  describe "GET #new" do
-    it "assigns a new status as @status" do
-      get :new
-      expect(assigns(:status)).to be_a_new(Status)
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested status as @status" do
-      status = Status.create! valid_attributes
-      get :edit, {:id => status.to_param}
-      expect(assigns(:status)).to eq(status)
-    end
-  end
-
   describe "POST #create" do
     context "with valid params" do
       it "creates a new Status" do
@@ -53,55 +30,6 @@ RSpec.describe StatusesController, type: :controller do
       end
     end
 
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) { {state: 0, message: 'I am down right now.'} }
-
-      it "updates the requested status" do
-        status = Status.create! valid_attributes
-        put :update, {:id => status.to_param, :status => new_attributes}
-        status.reload
-        expect(assigns(:status)).to eq(Status.last)
-      end
-
-      it "assigns the requested status as @status" do
-        status = Status.create! valid_attributes
-        put :update, {:id => status.to_param, :status => valid_attributes}
-        expect(assigns(:status)).to eq(status)
-      end
-
-      it "redirects to the status" do
-        status = Status.create! valid_attributes
-        put :update, {:id => status.to_param, :status => valid_attributes}
-        expect(response).to redirect_to(status)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns the status as @status" do
-        status = Status.create! valid_attributes
-        put :update, {:id => status.to_param, :status => invalid_attributes}
-        expect(assigns(:status)).to eq(status)
-      end
-
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested status" do
-      status = Status.create! valid_attributes
-      expect {
-        delete :destroy, {:id => status.to_param}
-      }.to change(Status, :count).by(-1)
-    end
-
-    it "redirects to the statuses list" do
-      status = Status.create! valid_attributes
-      delete :destroy, {:id => status.to_param}
-      expect(response).to redirect_to(statuses_url)
-    end
   end
 
 end
